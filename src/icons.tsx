0bbc274a417ab2f53c0ec1be8e40bec19057b572
nachorsanz <nachornsanz@gmail.com>
2022-12-02 14:46:42 +0100

    deploy v0.0.21

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,11 +1,10 @@
 
-import React from "react";
 import * as Icons from "react-icons-kit/fa";
-import { Icon } from 'react-icons-kit'
+
 
 export const getIcon   = ( iconName: string) => {
 const icon = Icons[iconName];
-return <Icon icon={icon} />;
+return icon
 }
 
 export const getAllIcons = () => {
