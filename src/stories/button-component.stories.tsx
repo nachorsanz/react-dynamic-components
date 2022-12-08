dba632cd726a0820744404b21038d746ec1f317b
Nacho Rodriguez Sanz <nachorsanz@gmail.com>
2022-12-08 17:33:00 +0100

    chore: update main

--- src/stories/button-component.stories.tsx
+++ src/stories/button-component.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
-
 import Button from '../ui/button/button'
 import { getAllColorIcons, getAllFAIcons } from '../domain/icons'
 
