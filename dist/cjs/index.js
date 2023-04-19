5fa91188f079b583d7be29379e1090c5390445fc
nachorsanz <nachorsanz@gmail.com>
2023-04-19 13:18:29 +0200

    chore: deploy v1.0.16-beta

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Icon = exports.Modal = exports.Input = exports.Button = void 0;
+exports.Switcher = exports.Select = exports.Icon = exports.Modal = exports.Input = exports.Button = void 0;
 // This file is used to export all the components from the library
 var button_1 = require("./ui/button-component/button");
 Object.defineProperty(exports, "Button", { enumerable: true, get: function () { return __importDefault(button_1).default; } });
@@ -13,4 +13,8 @@ var modal_1 = require("./ui/modal-component/modal");
 Object.defineProperty(exports, "Modal", { enumerable: true, get: function () { return __importDefault(modal_1).default; } });
 var icon_1 = require("./ui/icon-component/icon");
 Object.defineProperty(exports, "Icon", { enumerable: true, get: function () { return __importDefault(icon_1).default; } });
+var select_1 = require("./ui/select-component/select");
+Object.defineProperty(exports, "Select", { enumerable: true, get: function () { return __importDefault(select_1).default; } });
+var switcher_1 = require("./ui/switcher-component/switcher");
+Object.defineProperty(exports, "Switcher", { enumerable: true, get: function () { return __importDefault(switcher_1).default; } });
 //# sourceMappingURL=index.js.map
--- dist/esm/index.js
+++ dist/esm/index.js
@@ -3,4 +3,6 @@ export { default as Button } from './ui/button-component/button';
 export { default as Input } from './ui/input-component/input';
 export { default as Modal } from './ui/modal-component/modal';
 export { default as Icon } from './ui/icon-component/icon';
+export { default as Select } from './ui/select-component/select';
+export { default as Switcher } from './ui/switcher-component/switcher';
 //# sourceMappingURL=index.js.map
